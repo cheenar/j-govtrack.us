2bffa79f1a4b83aa3dba79b2b9cc5864bd8a05a2
Cheenar <wafflecaker@gmail.com>
2016-03-07 19:05:49 -0500

    Basic libraries

---
+++ src/com/github/cheenar/jgovtrack/GovTrack.java
@@ -0,0 +1,25 @@
+package com.github.cheenar.jgovtrack;
+
+/**
+ * @author Cheenar
+ * @description Main class for the JGovTrack API
+ */
+
+public class GovTrack
+{
+
+    public GovTrack()
+    {
+    }
+
+    public static String getAPIName()
+    {
+        return "JGovTrack";
+    }
+
+    public static String[] getAPIContributors()
+    {
+        return new String[] { "Cheenar" };
+    }
+
+}
